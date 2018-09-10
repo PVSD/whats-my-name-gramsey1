1919b3f909ec6fd68c3959ed80531d75857c944e
gramsey1 <graceramsey312@yahoo.com>
2018-09-10 11:10:02 -0400

    homework on monday.. really

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String fName = kbReader.nextLine();
+        System.out.println("What is your last name?");
+        String lName = kbReader.nextLine();
+        System.out.println("Your name is " + fName + " " + lName + ".");
     }
 }
